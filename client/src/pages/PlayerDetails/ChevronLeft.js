eca8cebc6fc203b54e4d7644668f57b6cbcaf7a8
lukabajic <lukabajic.dev@gmail.com>
2021-05-11 18:14:57 +0200

    Add chevron svg

---
+++ client/src/pages/PlayerDetails/ChevronLeft.js
@@ -0,0 +1,19 @@
+const ChevronLeft = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    aria-hidden="true"
+    focusable="false"
+    width="0.66em"
+    height="1em"
+    preserveAspectRatio="xMidYMid meet"
+    viewBox="0 0 1088 1664"
+  >
+    <path
+      d="M1043 301L512 832l531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19L45 877q-19-19-19-45t19-45L787 45q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z"
+      fill="#ffffff"
+    />
+  </svg>
+);
+
+export default ChevronLeft;

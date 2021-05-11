1cdce2fb5a2db7efe0e2f3046123475023a5a7e3
lukabajic <lukabajic.dev@gmail.com>
2021-05-11 11:30:51 +0200

    Make request to proxy

--- client/src/pages/Index/index.js
+++ client/src/pages/Index/index.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+
+import { getPlayers } from "../../store/actions/players";
 
 import "./styles.scss";
 
-const Index = () => {
+const Index = ({ players, loading, error, getPlayers }) => {
+  useEffect(() => {
+    if (!players) {
+      getPlayers();
+    }
+  }, [players, getPlayers]);
+
+  if (loading) return <div>Loading</div>;
+
+  if (error) return <div>Erorr</div>;
+
   return <div>Index</div>;
 };
 
-export default Index;
+export default connect(
+  (state) => ({
+    players: state.players.players,
+    loading: state.players.loading,
+    error: state.players.error,
+  }),
+  {
+    getPlayers,
+  }
+)(Index);
--- client/src/store/actions/players.js
+++ client/src/store/actions/players.js
@@ -1,4 +1,3 @@
-import e from "express";
 import * as actionTypes from "./actionTypes";
 
 const getPlayersStart = () => ({ type: actionTypes.GET_PLAYERS_START });

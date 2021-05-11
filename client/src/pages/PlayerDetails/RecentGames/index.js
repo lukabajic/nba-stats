1663531478e5f797188b19b19ccfb8eef18ad17e
lukabajic <lukabajic.dev@gmail.com>
2021-05-11 18:27:54 +0200

    Move recent games

--- client/src/pages/PlayerDetails/RecentGames/index.js
+++ client/src/pages/PlayerDetails/RecentGames/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import cn from "classnames";
 
-import teams from "../../utls/teams";
-import { Spinner } from "../../components/Loader";
+import teams from "../../../utls/teams";
+import { Spinner } from "../../../components/Loader";
 
 import "./styles.scss";
 

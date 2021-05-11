7a02d9710a8619d1a50a523defad6ba2f3c6daea
lukabajic <lukabajic.dev@gmail.com>
2021-05-11 11:15:29 +0200

    Add redirect

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 import Index from "./pages/Index";
 import Player from "./pages/Player";
@@ -9,6 +14,7 @@ const App = () => (
     <Switch>
       <Route path="/" exact component={Index} />
       <Route path="/player/:id" exact component={Player} />
+      <Redirect to="/" />
     </Switch>
   </Router>
 );

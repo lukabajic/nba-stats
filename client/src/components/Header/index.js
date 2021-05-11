9ae2f086eeb423d09d03eed0a777f92bef4ded77
lukabajic <lukabajic.dev@gmail.com>
2021-05-11 15:05:33 +0200

    Rework header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -5,14 +5,16 @@ import "./styles.scss";
 
 const Header = ({ displayName }) => (
   <header className="Header">
-    <Link to="/" className="Header__logo">
-      <img
-        src={process.env.PUBLIC_URL + "/logo.svg"}
-        alt="NBA logo"
-        className="Header__logo-img"
-      />
-    </Link>
-    <h1 className="Header__text">{displayName || "Players"}</h1>
+    <div className="Header__left">
+      <Link to="/" className="Header__logo">
+        <img
+          src={process.env.PUBLIC_URL + "/logo.svg"}
+          alt="NBA logo"
+          className="Header__logo-img"
+        />
+      </Link>
+      <h1 className="Header__text">{displayName || "Players"}</h1>
+    </div>
   </header>
 );
 

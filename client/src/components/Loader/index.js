f106364397c2e97e188821eace3bfbad20b01601
lukabajic <lukabajic.dev@gmail.com>
2021-05-11 18:14:05 +0200

    Reuse loader

--- client/src/components/Loader/index.js
+++ client/src/components/Loader/index.js
@@ -1,10 +1,21 @@
 import React from "react";
+import cn from "classnames";
 
 import "./styles.scss";
 
+export const Spinner = ({ blue }) => (
+  <div
+    className={cn("Loader", {
+      "Loader--blue": blue,
+    })}
+  >
+    Loading...
+  </div>
+);
+
 const Loader = () => (
   <div className="Overlay">
-    <div className="Loader">Loading...</div>
+    <Spinner />
   </div>
 );
 
